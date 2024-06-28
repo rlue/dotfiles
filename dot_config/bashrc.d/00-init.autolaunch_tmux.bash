584707f476df3f71c9cafb3297a86603f4c75ac6
Ryan Lue <hello@ryanlue.com>
2024-06-28 13:00:43 -0700

    Fix tmux config (esp. true color in alacritty)

--- dot_config/bashrc.d/00-init.autolaunch_tmux.bash
+++ dot_config/bashrc.d/00-init.autolaunch_tmux.bash
@@ -1,6 +1,8 @@
 if [ -z "$SSH_CONNECTION" ] && [ "$XDG_SESSION_TYPE" != tty ]; then
   if [ -z "$TMUX" ]; then
-    if infocmp xterm-256color-italic >/dev/null 2>&1; then
+    if [ -n "$ALACRITTY_WINDOW_ID" ] && infocmp alacritty >/dev/null 2>&1; then
+      export TERM=alacritty
+    elif infocmp xterm-256color-italic >/dev/null 2>&1; then
       export TERM=xterm-256color-italic
     fi
 
@@ -15,7 +17,7 @@ if [ -z "$SSH_CONNECTION" ] && [ "$XDG_SESSION_TYPE" != tty ]; then
     fi
   else # initialize tmux plugins (if possible)
     if [ "$(uname)" = Linux ] || hash greadlink >/dev/null 2>&1; then
-      source "$HOME/.config/tmux/tmux-git/tmux-git.sh" >/dev/null 2>&1
+      source "$HOME/.local/bin/tmux-git.sh" >/dev/null 2>&1
     fi
   fi
 fi
